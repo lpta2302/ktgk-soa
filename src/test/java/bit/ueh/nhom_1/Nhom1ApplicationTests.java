83960de06975ca15651104f14f3dfc639484df34
lpta2302 <lpta2302@gmail.com>
2025-03-11 13:45:26 +0700

    first commit

---
+++ src/test/java/bit/ueh/nhom_1/Nhom1ApplicationTests.java
@@ -0,0 +1,13 @@
+package bit.ueh.nhom_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nhom1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
